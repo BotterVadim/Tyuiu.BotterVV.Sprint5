f80a0e7f10cb63931b75a62b8f0f8014d1561842
Tyuiu.BotterVV@outlook.com <Tyuiu.BotterVV@outlook.com>
2023-11-24 18:40:40 +0500

    Task6

---
+++ Tyuiu.BotterVV.Sprint5.Task6.V4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BotterVV.Sprint5.Task6.V4.Lib;
+
+
+namespace Tyuiu.BotterVV.Sprint5.Task6.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Боттер В.В. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Боттер Вадим Валерьевич | ПКТб-23-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл InPutDataFileTask6V4.txt в котором есть набор символьных       *");
+            Console.WriteLine("* данных.  Найти количество заглавных русских букв в заданной строке.     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask6V4.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Количество заглавных русских букв: " + ds.LoadFromDataFile(path));
+            Console.ReadKey();
+        }
+    }
+}
